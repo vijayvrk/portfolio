ad17300736383e605579611b74637399dd236ea8
VIJAYA RAGAVAN.K <139204254+vijayvrk@users.noreply.github.com>
2024-09-29 20:45:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ========================= typing animation ========================== */
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Student"],
+    typeSpeed: 60,
+    backSpeed: 30,   
+    backDelay: 1000,  
+    startDelay: 500, 
+    loop: true,
+    fadeOut: true,   
+    fadeOutClass: "fade-out" 
+});
